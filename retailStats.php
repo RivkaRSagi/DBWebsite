00ec8a4523e0eaff64ad4fb57467c06db869d3a3
Kevin Jacob <KevinJacob10000@gmail.com>
2024-11-23 20:55:57 -0500

    updated exportData and added Sales Log export button to retailStats.php

--- retailStats.php
+++ retailStats.php
@@ -1,5 +1,6 @@
 <?php
 include 'db.php';
+include 'exportData.php';
 session_start();
 
 if($_SERVER['REQUEST_METHOD']==='POST'){
@@ -8,6 +9,27 @@ if($_SERVER['REQUEST_METHOD']==='POST'){
         header("Location: login.html");
         exit;
     }
+}
+
+if (isset($_POST['exportsales'])) {    
+    $query = "SELECT * FROM ItemSales";
+    $result = $conn->query($query);
+
+    if ($result->num_rows > 0) {
+        $data = [];
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+    }
+
+    //get columns
+    $columns = [];
+    $fieldInfo = $result->fetch_fields();
+    foreach ($fieldInfo as $field) {
+        $columns[] = $field->name;
+    }
+    
+    exportToCSV($columns, $data, "Sales Log");
 }
 
 ?>
@@ -37,27 +59,33 @@ if($_SERVER['REQUEST_METHOD']==='POST'){
     </div>
     <div class="bodyDiv">
     <div class="majorDiv">
-            <h3>Item Sales</h3>
-            <div class="minorDiv">
-                <table>
-                    <tr>
-                        <th>Item Name</th>
-                        <th>Quantity</th>
-                        <th>SaleID</th>
-                    </tr>
-                    <tr>
-                        <td>fetch from db here</td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                </table>
-            </div>
+        <h3>Item Sales</h3>
+        <div class="minorDiv">
+            <table>
+                <tr>
+                    <th>Item Name</th>
+                    <th>Quantity</th>
+                    <th>SaleID</th>
+                </tr>
+                <tr>
+                    <td>fetch from db here</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </table>
+        </div>         
+    </div>
+
+    <!-- Export Sales Log Form -->
+    <form action="retailStats.php" method="POST">
+        <button type="submit" name="exportsales">Export Sales Log</button>
+    </form>
+
+    <div class="majorDiv">
+        <h3>Statistics</h3>
+        <div class="minorDiv">
         </div>
-        <div class="majorDiv">
-            <h3>Statistics</h3>
-            <div class="minorDiv">
-            </div>
-        </div>
+    </div>
     </div>
 </body>
 </html>
